e16cfe709c8da675f866b77941ca400dbe1b3022
kann181 <kannan024@gmail.com>
2017-01-29 00:12:13 +0530

    some commit

---
+++ chapter1/concurrent.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func count() {
+    for i:=0; i< 5; i++ {
+        fmt.Println(i)
+        time.Sleep(time.Millisecond * 1)
+    }
+}
--- chapter1/main.go
+++ chapter1/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func getName() string {
 	return "world!"
@@ -9,4 +12,15 @@ func getName() string {
 func main() {
 	name := getName()
 	fmt.Println("Hello ", name)
+
+	concurrent()
+
+}
+
+func concurrent() {
+	go count()
+
+	time.Sleep(time.Millisecond * 2)
+	fmt.Println("Hello World")
+	time.Sleep(time.Millisecond * 5)
 }
